3261d76b71ffd94529a34fe363825d77f913d68e
Sarah Nnanyelugo <sarahnnanyelugo@gmail.com>
2024-12-14 11:26:05 +0100

    subjects

---
+++ .history/src/components/MobileTable/MobileTable_20241214105559.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import "./mobile-table.scss";
+import { GoDotFill } from "react-icons/go";
+import { Link } from "react-router-dom";
+
+export const MobileTable = ({ data, isOdd }) => {
+  return (
+    <>
+      <Link
+        to={"/view-voting"}
+        className={`mobile-table-container ${isOdd ? "odd-row" : "even-row"}`}
+      >
+        <h6>{data.heading}</h6>
+        <div className="d-flex">
+          <div style={{ flexGrow: 1 }}>
+            <small>
+              Start:
+              <span> {data.startDate}</span>
+              {data.startingTime}
+            </small>
+            <br />
+            <small>
+              End:
+              <span> {data.endDate}</span>
+              {data.endingTime}
+            </small>
+          </div>
+          <Col>
+            <button>
+              <GoDotFill />
+              {data.status}
+            </button>
+          </Col>
+        </div>
+      </Link>
+    </>
+  );
+};
---
+++ .history/src/components/MobileTable/MobileTable_20241214110730.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import "./mobile-table.scss";
+import { GoDotFill } from "react-icons/go";
+import { Link } from "react-router-dom";
+
+export const MobileTable = ({ data, isOdd }) => {
+  const getStatusClass = (status) => {
+    const normalizedStatus = status.trim().toLowerCase();
+    if (normalizedStatus === "conducted") {
+      return "status-conducted";
+    } else if (normalizedStatus === "published") {
+      return "status-published";
+    } else {
+      return "status-default";
+    }
+  };
+  return (
+    <>
+      <Link
+        to={"/view-voting"}
+        className={`mobile-table-container ${isOdd ? "odd-row" : "even-row"}`}
+      >
+        <h6>{data.heading}</h6>
+        <div className="d-flex">
+          <div style={{ flexGrow: 1 }}>
+            <small>
+              Start:
+              <span> {data.startDate}</span>
+              {data.startingTime}
+            </small>
+            <br />
+            <small>
+              End:
+              <span> {data.endDate}</span>
+              {data.endingTime}
+            </small>
+          </div>
+          <Col>
+            <button>
+              <GoDotFill />
+              {data.conductStatus}
+            </button>{" "}
+            <button>
+              <GoDotFill />
+              {data.publishedStatus}
+            </button>
+          </Col>
+        </div>
+      </Link>
+    </>
+  );
+};
